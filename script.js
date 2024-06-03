480d56a0b3787c48a1f4012281aac4b14588e51a
Iren-15 <131612844+Iren-15@users.noreply.github.com>
2024-06-04 02:14:00 +0300

    cats cafe

---
+++ script.js
@@ -0,0 +1,57 @@
+
+gsap.from(".navbar", {
+    y: 50,
+    delay: 1,
+    duration: 0.8,
+    opacity: 0
+  });
+
+  gsap.from(".my-footer-list", {
+    y: 20,
+    delay: 1,
+    duration: 0.8,
+    opacity: 0
+  });
+
+  gsap.from (".photo-one", {
+    x: -50,
+    delay: 0.2,
+    duration: 0.8,
+    opacity: 0
+  });
+
+  gsap.from (".photo-three", {
+    x: -50,
+    delay: 0.2,
+    duration: 0.8,
+    opacity: 0
+  });
+
+  gsap.from (".photo-two", {
+    x: 50,
+    delay: 0.2,
+    duration: 0.8,
+    opacity: 0
+  });
+
+  gsap.from (".photo-four", {
+    x: 50,
+    delay: 0.2,
+    duration: 0.8,
+    opacity: 0
+  });
+
+  gsap.from (".par", {
+    scale: -1,
+    delay: 1.8,
+    duration: 1,
+    opacity: 0
+  }) 
+
+  gsap.from (".btn", {
+    opacity: 0,
+    delay: 3.2,
+    duration: 1.5
+  })
+
+
